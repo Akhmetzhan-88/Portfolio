b849bd00cb096aed8f5969e2ec1e29949a4d403f
Akhmetzhan-88 <bauyrzhanahmetuly@gmail.com>
2025-02-24 18:40:21 +0500

    Delete Yeezy.js

--- Yeezy.js
+++
@@ -1,9 +0,0 @@
-const toggleButton = document.getElementById("darkModeToggle");
-const body = document.body;
-
-if (localStorage.getItem("darkMode") === "enabled") {
-    body.classList.add("dark-mode");
-}
-toggleButton.addEventListener("click", () => {
-    body.classList.toggle("dark-mode");
-});
